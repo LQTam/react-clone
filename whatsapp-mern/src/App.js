7fa924811530307bf941abd8bf68cefe5d905326
TamLQ <laquyettam1995@gmail.com>
2021-06-27 16:15:59 +0700

    ADD: axios, pusher-js

--- whatsapp-mern/src/App.js
+++ whatsapp-mern/src/App.js
@@ -1,8 +1,33 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Chat from "./components/Chat";
 import Sidebar from "./components/Sidebar";
+import Pusher from "pusher-js";
+import axios from "./axios";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    axios.get("/messages/sync").then((response) => {
+      setMessages(response.data);
+    });
+  }, []);
+
+  useEffect(() => {
+    var pusher = new Pusher("56d23b108dfd4f5ecf4c", {
+      cluster: "ap1",
+    });
+
+    var channel = pusher.subscribe("messages");
+    channel.bind("inserted", function (newMessage) {
+      setMessages([...messages, newMessage]);
+    });
+
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
   return (
     <div className="app">
       <div className="app__body">
