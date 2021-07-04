7fe687b616e54c83d563d3c105f08e0f3cadb832
TamLQ <laquyettam1995@gmail.com>
2021-07-05 04:44:47 +0700

    ADD AND UPDATE: clean code server.js add s3 enpoint, firebase admin

--- server.js
+++ server.js
@@ -1,33 +1,25 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import multer from "multer";
-import { GridFsStorage } from "multer-gridfs-storage";
-import Grid from "gridfs-stream";
 import bodyParser from "body-parser";
-import path from "path";
-import Pusher from "pusher";
-import mongoPosts from "./postModel.js";
-
-Grid.mongo = mongoose.mongo;
+import "dotenv/config";
+import postRoute from "./routes/posts.js";
+import imageRoute from "./routes/images.js";
+import userRoute from "./routes/users.js";
+import { pusher } from "./config/pusher.js";
+import helmet from "helmet";
 
 // app config
 const app = express();
 const port = process.env.PORT || 9000;
-const pusher = new Pusher({
-  appId: "1226653",
-  key: "cef11e57182d122d1edb",
-  secret: "ad7dabbda5ccf94fc4b9",
-  cluster: "ap1",
-  useTLS: true,
-});
 
 //middleware
 app.use(bodyParser.json());
 app.use(cors());
+app.use(helmet());
 
 // db config
-const mongoURI = process.env.MONGODB_URI;
+const mongoURI = process.env.MONGODB_URI || process.env.MONGODB_URI_LOCAL;
 const conn = mongoose.createConnection(mongoURI, {
   useCreateIndex: true,
   useNewUrlParser: true,
@@ -39,27 +31,21 @@ mongoose.connect(mongoURI, {
   useUnifiedTopology: true,
 });
 
-let gfs;
-// let gridFSBucket
 conn.once("open", () => {
   console.log("DB Connected");
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection("images");
-  // gridFSBucket = new mongoose.mongo.GridFSBucket(conn.db,{bucketName:'images'})
-
-  // const changeStream = mongoose.connection.collection('posts').watch();
   const changeStream = conn.db.collection("posts").watch();
   changeStream.on("change", (change) => {
-    console.log(change);
     if (change.operationType === "insert") {
-      let { user, imgName, text, avatar, timestamp, uid } = change.fullDocument;
+      let { _id } = change.documentKey;
+      let { user, text, avatar, timestamp, uid, images } = change.fullDocument;
       pusher.trigger("posts", "inserted", {
         user,
-        imgName,
+        _id,
         text,
         avatar,
         timestamp,
         uid,
+        images,
       });
     } else if (change.operationType === "delete") {
       let { _id } = change.documentKey;
@@ -70,92 +56,10 @@ conn.once("open", () => {
   });
 });
 
-const storage = new GridFsStorage({
-  url: mongoURI,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      const filename = `image-${Date.now()}${path.extname(file.originalname)}`;
-      const fileInfo = {
-        filename,
-        bucketName: "images",
-      };
-
-      resolve(fileInfo);
-    });
-  },
-});
-
-const upload = multer({ storage });
-
 //api routes
 app.get("/", (req, res) => res.status(200).send("Hello World!"));
-
-app.post("/upload/image", upload.single("file"), (req, res) => {
-  console.log(req.file);
-  res.status(201).send(req.file);
-});
-
-app.post("/upload/post", (req, res) => {
-  const dbPost = req.body;
-  mongoPosts.create(dbPost, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      console.log(data);
-      res.status(201).send(data);
-    }
-  });
-});
-
-app.get("/retrieve/posts", (req, res) => {
-  mongoPosts.find((err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      data.sort((b, a) => {
-        return a.timestamp - b.timestamp;
-      });
-      res.status(200).send(data);
-    }
-  });
-});
-
-app.delete("/posts/:postId/delete", function (req, res) {
-  let { postId } = req.params;
-  mongoPosts.deleteOne({ _id: postId }, function (err, data) {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.status(200).send(data);
-    }
-  });
-});
-
-app.get("/retrieve/images/single", (req, res) => {
-  gfs.files.findOne({ filename: req.query.name }, (err, file) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      if (!file || file.length === 0)
-        res.status(404).json({ err: "file not found." });
-      else {
-        const readStream = gfs.createReadStream(file.filename);
-        // const readStream = gridFSBucket.openDownloadStream(file.filename);
-        readStream.pipe(res);
-      }
-    }
-  });
-});
-
-app.delete("/images/:imgName/delete", function (req, res) {
-  let { imgName } = req.params;
-  gfs.files.deleteOne({ filename: imgName }, function (err, data) {
-    if (err) res.status(500).send(err);
-    else {
-      res.status(200).send(data);
-    }
-  });
-});
-
+app.use("/", postRoute);
+app.use("/", imageRoute);
+app.use("/", userRoute);
 // listen
-app.listen(port, () => console.log(`Server connected on localhost:${port}`));
+app.listen(port, () => console.log(`Server connected on localhost:${port} `));
