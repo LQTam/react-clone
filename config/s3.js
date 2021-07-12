c248ce0e9c617e8cf2c4636d3403537731f17e8b
TamLQ <laquyettam1995@gmail.com>
2021-07-13 04:40:17 +0700

    REMOVE submodule facebook-mern-backend

--- config/s3.js
+++
@@ -1,7 +0,0 @@
-import AWS from "aws-sdk";
-
-export const s3 = new AWS.S3({
-  accessKeyId: process.env.AWSAccessKeyId,
-  secretAccessKey: process.env.AWSSecretKey,
-  region: process.env.AWSRegion,
-});
--- config/variables.js
+++
@@ -1,4 +0,0 @@
-import expressModule from "express";
-
-export const BUCKET_NAME = process.env.BUCKET_NAME;
-export const express = expressModule;
