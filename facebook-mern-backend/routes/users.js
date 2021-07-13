0a223715c622c15505358438c392e7dde15684b5
TamLQ <laquyettam1995@gmail.com>
2021-07-13 17:51:21 +0700

    MODIFY: CSS reponsive to 320px

---
+++ facebook-mern-backend/routes/users.js
@@ -0,0 +1,23 @@
+import admin from "firebase-admin";
+import { Router } from "express";
+import serviceAccount from "../facebook-mern-3c59a-firebase-adminsdk-s49tf-19587a0de4.json";
+
+const router = Router();
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const records = 1000;
+const listAllUsers = (nextPageToken) => {
+  return admin.auth().listUsers(records, nextPageToken);
+};
+router.get("/users", async (req, res) => {
+  try {
+    const { users } = await listAllUsers();
+    res.send({ users });
+  } catch (err) {
+    console.log("Error listUsers: ", err);
+    res.status(500).send(err);
+  }
+});
+export default router;
